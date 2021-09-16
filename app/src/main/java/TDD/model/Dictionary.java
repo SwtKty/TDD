c4f81fa5999edfdb60784820e92fd5509b3e44d4
Cathy Siwatala <cathy.siwatala@gmail.com>
2021-09-16 18:53:09 +0200

    Triangulation.Question6

--- app/src/main/java/TDD/model/Dictionary.java
+++ app/src/main/java/TDD/model/Dictionary.java
@@ -24,14 +24,17 @@ public class Dictionary {
         }
     }
 
+    Map<String, String> translations = new HashMap<>();
+
     public String addTranslation (String mot1, String mot2){
         this.mot2=mot2;
-        return mot2;
+        this.mot1=mot1;
+        translations.put(mot1,mot2);
+        return translations.get(mot1);
     }
 
     public String getTranslation (String mot1){
-        return mot2;
+        return translations.get(mot1);
     }
 
-    Map<String, String> translations = new HashMap<>();
 }
