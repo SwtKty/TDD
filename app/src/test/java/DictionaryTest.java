cb56398b98e71d0ac6fc5ca8030f78720129df1e
Cathy Siwatala <cathy.siwatala@gmail.com>
2021-09-16 19:35:47 +0200

    Fixtures_de_JUnit.Question1

--- app/src/test/java/DictionaryTest.java
+++ app/src/test/java/DictionaryTest.java
@@ -1,4 +1,6 @@
 import TDD.model.Dictionary;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,7 +12,17 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class DictionaryTest {
 
-    public Dictionary dict = new Dictionary("Example");
+    public Dictionary dict;
+
+    @Before
+    public void initialize(){
+        dict = new Dictionary("Example");
+    }
+
+    @After
+    public void initialize2 (){
+        dict = null;
+    }
 
     @Test
     public void testDictionaryName() {
