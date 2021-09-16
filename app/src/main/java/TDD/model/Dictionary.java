2585040bdab68012817fd8f29bd7471a49e09a27
Cathy Siwatala <cathy.siwatala@gmail.com>
2021-09-16 15:18:02 +0200

    question4

---
+++ app/src/main/java/TDD/model/Dictionary.java
@@ -0,0 +1,10 @@
+package TDD.model;
+
+public class Dictionary {
+    public Dictionary() {
+    }
+
+    public String getName(){
+        return "Example";
+    }
+}
--- app/src/test/java/DictionaryTest.java
+++ app/src/test/java/DictionaryTest.java
@@ -1,12 +1,20 @@
+import TDD.model.Dictionary;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.Dictionary;
+import java.util.Enumeration;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class DictionaryTest {
 
+    public Dictionary dict = new Dictionary();
+
     @Test
-    public void testDictionaryName(){
+    public void testDictionaryName() {
+
+        assertThat(dict.getName(), equalTo("Example"));
     }
 }
